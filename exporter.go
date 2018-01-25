2a38a9a83fddb2287004c2a3e9cf3867b23661b7
Jon Stevens <latchkey@gmail.com>
2018-01-25 19:12:51 +0700

    bit of cleanup of del's pr and add more of the time metrics in case they are ever useful one day

--- exporter.go
+++ exporter.go
@@ -10,6 +10,7 @@ import (
         "strconv"
 )
 
+//
 func newGaugeMetric(metricName string, docString string, labels []string) *prometheus.GaugeVec {
 	return prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
@@ -20,24 +21,25 @@ func newGaugeMetric(metricName string, docString string, labels []string) *prome
 	)
 }
 
-// Define the internal structure of the Exporter class
+// Collector interface
 type Exporter struct {
-	hostname      string
-	port          int64
-	timeout       time.Duration
-	chipStatGauge *prometheus.GaugeVec
-	devsHashrateGauge  *prometheus.GaugeVec
-	devsAcceptedGauge  *prometheus.GaugeVec
-	devsRejectedGauge  *prometheus.GaugeVec
-	devsErrorsGauge    *prometheus.GaugeVec
+	hostname			string
+	port				int64
+	timeout				time.Duration
+	chipStatGauge		*prometheus.GaugeVec
+	devsHashRateGauge	*prometheus.GaugeVec
+	devsHashCountGauge	*prometheus.GaugeVec
+	devsErrorsGauge		*prometheus.GaugeVec
 	sync.Mutex
 }
 
+//
 type CgminerStats struct {
-	ChipStat *cgminer.ChipStat
-	Devs *[]cgminer.Devs
+	ChipStat	*cgminer.ChipStat
+	Devs		*[]cgminer.Devs
 }
 
+//
 func (e *Exporter) fetchData() (*CgminerStats, error) {
 	log.Printf("fetchData(%s:%d, %s)", e.hostname, e.port, e.timeout)
 
@@ -58,65 +60,70 @@ func (e *Exporter) fetchData() (*CgminerStats, error) {
 	return &CgminerStats{chipStat, devs}, nil
 }
 
+//
 func NewExporter(cgHost string, cgPort int64, cgTimeout time.Duration) (*Exporter, error) {
 	return &Exporter{
-		hostname:      cgHost,
-		port:          cgPort,
-		timeout:       cgTimeout,
-		chipStatGauge: newGaugeMetric("chip_total", "How many accept/reject on a chip", []string{"id", "state"}),
-		devsHashrateGauge:     newGaugeMetric("devs_hashrate", "Device Hashrate", []string{"id"}),
-		devsAcceptedGauge:     newGaugeMetric("devs_accepted", "Device Hash Accepted", []string{"id"}),
-		devsRejectedGauge:     newGaugeMetric("devs_rejected", "Device Hash Rejected", []string{"id"}),
-		devsErrorsGauge:       newGaugeMetric("devs_errors", "Device Hardware Errors", []string{"id"}),
+		hostname:			cgHost,
+		port:				cgPort,
+		timeout:			cgTimeout,
+		chipStatGauge:		newGaugeMetric("chip_total", "Chip accept/reject total", []string{"id", "state"}),
+		devsHashCountGauge:	newGaugeMetric("devs_hashcount_total", "Device hash accept/reject total", []string{"id", "state"}),
+		devsHashRateGauge:	newGaugeMetric("devs_hashrate_total", "Device hashrate total", []string{"id", "rate"}),
+		devsErrorsGauge:	newGaugeMetric("devs_errors_total", "Device hardware errors total", []string{"id"}),
 	}, nil
 }
 
-// This function is used to provide comments in the HTTP output
-func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	e.chipStatGauge.Describe(ch)
-	e.devsHashrateGauge.Describe(ch)
-	e.devsAcceptedGauge.Describe(ch)
-	e.devsRejectedGauge.Describe(ch)
-	e.devsErrorsGauge.Describe(ch)
-}
-
-// This is the main function that gets called on a HTTP request, to produce the collected
-// output data.
-func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	e.Lock()
-	defer e.Unlock()
-
-        // cgStats is the combined structure read from the device by the fetchData() function
-        // It has a ChipStat and a Devs element.
-	cgStats, err := e.fetchData()
-	if err != nil {
-		return
-	}
-	
-	// Read the data from the ChipStat element and parse into the chipStatGauge array.
+// Parses the map[string]float64 into a gauge with labels.
+// {"1_accept": 1, "1_reject": 100}
+func collectChipStat(e *Exporter, cgStats *CgminerStats, ch chan<- prometheus.Metric) {
 	chipStat := cgStats.ChipStat
 	for chip, value := range *chipStat {
 		s := strings.Split(chip, "_")
 		chipId, chipType := s[0], s[1]
 		e.chipStatGauge.WithLabelValues(chipId, chipType).Set(value)
 	}
-	
-	// Read the data from the Devs element and parse into 4 separate arrays for
-	// hashrate, accepted rate, rejected rate and hardware errors.
+
+	e.chipStatGauge.Collect(ch)
+}
+
+//
+func collectDevs(e *Exporter, cgStats *CgminerStats, ch chan<- prometheus.Metric) {
 	devs := cgStats.Devs
 	for id, value := range *devs {
-		e.devsHashrateGauge.WithLabelValues(strconv.Itoa(id)).Set(value.MHS5s)
-		e.devsAcceptedGauge.WithLabelValues(strconv.Itoa(id)).Set(float64(value.Accepted))
-		e.devsRejectedGauge.WithLabelValues(strconv.Itoa(id)).Set(float64(value.Rejected))
-		e.devsErrorsGauge.WithLabelValues(strconv.Itoa(id)).Set(float64(value.HardwareErrors))
-		// log.Printf("dev %d, %3.2f", id, value.MHS5s)
+		idStr := strconv.Itoa(id)
+		e.devsHashCountGauge.WithLabelValues(idStr, "accept").Set(float64(value.Accepted))
+		e.devsHashCountGauge.WithLabelValues(idStr, "reject").Set(float64(value.Rejected))
+		e.devsHashRateGauge.WithLabelValues(idStr, "MHS_5s").Set(value.MHS5s)
+		e.devsHashRateGauge.WithLabelValues(idStr, "MHS_1m").Set(value.MHS1m)
+		e.devsHashRateGauge.WithLabelValues(idStr, "MHS_5m").Set(value.MHS5m)
+		e.devsHashRateGauge.WithLabelValues(idStr, "MHS_15m").Set(value.MHS15m)
+		e.devsErrorsGauge.WithLabelValues(idStr).Set(float64(value.HardwareErrors))
 	}
 
-	// This causes the gauges to be emitted on the HTTP output.
-	e.chipStatGauge.Collect(ch)
-	e.devsHashrateGauge.Collect(ch)
-	e.devsAcceptedGauge.Collect(ch)
-	e.devsRejectedGauge.Collect(ch)
+	e.devsHashRateGauge.Collect(ch)
+	e.devsHashCountGauge.Collect(ch)
 	e.devsErrorsGauge.Collect(ch)
 }
 
+//
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	e.chipStatGauge.Describe(ch)
+	e.devsHashRateGauge.Describe(ch)
+	e.devsHashCountGauge.Describe(ch)
+	e.devsErrorsGauge.Describe(ch)
+}
+
+//
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	e.Lock()
+	defer e.Unlock()
+
+	// Fetch data from the worker
+	cgStats, err := e.fetchData()
+	if err != nil {
+		return
+	}
+
+	collectChipStat(e, cgStats, ch)
+	collectDevs(e, cgStats, ch)
+}
