6a0177f70b470312106ff294ea36ebcc8d41b5f3
Jon Stevens <latchkey@gmail.com>
2018-01-27 17:20:27 +0700

    simplify the install script

--- install.sh
+++ install.sh
@@ -18,22 +18,8 @@ fi
 dowork() {
 	ipaddr=$1
 	echo "----------- ${ipaddr} start"
-	sshpass -e ssh -o StrictHostKeychecking=no root@$ipaddr "echo '
-[Unit]
-Description=bw_exporter
-After=init.service
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/bw_exporter
-Restart=always
-RestartSec=4s
-StandardOutput=journal+console
-
-[Install]
-WantedBy=multi-user.target
-' > /etc/systemd/system/bw_exporter.service; sync; sync;"
 	sshpass -e scp -o StrictHostKeychecking=no bw_exporter root@$ipaddr:/usr/bin
+	sshpass -e scp -o StrictHostKeychecking=no bw_exporter.service root@$ipaddr:/etc/systemd/system
 	sshpass -e ssh -o StrictHostKeychecking=no root@$ipaddr 'chmod ugo+x /usr/bin/bw_exporter; systemctl enable bw_exporter; systemctl start bw_exporter;'
 	echo "----------- ${ipaddr} finish"
 }
